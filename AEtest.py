8c68b40444d049c7a5233e1b2db0469087b00587
Yiftach Kolb <zelhar@gmail.com>
2021-11-19 19:22:23 +0100

    S

---
+++ AEtest.py
@@ -0,0 +1,131 @@
+import argparse
+import torch
+import torch.utils.data
+
+from torch.utils.data import Dataset, DataLoader, TensorDataset
+
+from torch import nn, optim
+from torch.nn import functional as F
+from torchvision import datasets, transforms
+from torchvision.utils import save_image
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+batch_size = 120
+log_interval = 100
+epochs = 10
+seed = 42
+
+fakedata = torch.rand((5000, 1, 28, 28))
+fakedataset = TensorDataset(fakedata)
+x = fakedataset.__getitem__(0)
+x
+
+faketrain = fakedata[:4500]
+faketest = fakedata[4500:]
+traindata = TensorDataset(faketrain)
+testdata = TensorDataset(faketest)
+
+mnist = datasets.MNIST(
+    "../data", train=True, download=True, transform=transforms.ToTensor()
+)
+
+train_loader = torch.utils.data.DataLoader(
+    datasets.MNIST(
+        "../data", train=True, download=True, transform=transforms.ToTensor()
+    ),
+    batch_size=batch_size,
+    shuffle=True,
+)
+
+test_loader = torch.utils.data.DataLoader(
+    datasets.MNIST('../data', train=False, transform=transforms.ToTensor()),
+    batch_size=batch_size, shuffle=True)
+
+train_loader = DataLoader(traindata, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(testdata, batch_size=batch_size, shuffle=True)
+
+class AE(nn.Module):
+    def __init__(self):
+        super(AE, self).__init__()
+        self.fc1 = nn.Linear(784, 400)
+        self.fc2 = nn.Linear(400, 20)
+        self.fc3 = nn.Linear(20, 400)
+        self.fc4 = nn.Linear(400, 784)
+
+    def encode(self, x):
+        h1 = F.relu(self.fc1(x))
+        return self.fc2(h1)
+
+    def decode(self, z):
+        h3 = F.relu(self.fc3(z))
+        return torch.sigmoid(self.fc4(h3))
+
+    def forward(self, x):
+        z = self.encode(x.view(-1, 784))
+        return self.decode(z)
+
+
+model = AE().to(device)
+optimizer = optim.Adam(model.parameters(), lr=1e-3)
+
+
+# Reconstruction + KL divergence losses summed over all elements and batch
+def loss_function(recon_x, x):
+    BCE = F.binary_cross_entropy(recon_x, x.view(-1, 784), reduction='sum')
+    # see Appendix B from VAE paper:
+    # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
+    # https://arxiv.org/abs/1312.6114
+    # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
+    # KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+    return BCE
+
+
+def train(epoch):
+    model.train()
+    train_loss = 0
+    for batch_idx, (data, ) in enumerate(train_loader):
+        data = data.to(device)
+        optimizer.zero_grad()
+        recon_batch = model(data)
+        loss = loss_function(recon_batch, data)
+        loss.backward()
+        train_loss += loss.item()
+        optimizer.step()
+        if batch_idx % log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader),
+                loss.item() / len(data)))
+    print('====> Epoch: {} Average loss: {:.4f}'.format(
+          epoch, train_loss / len(train_loader.dataset)))
+
+
+def test(epoch):
+    model.eval()
+    test_loss = 0
+    with torch.no_grad():
+        for i, (data, ) in enumerate(test_loader):
+            data = data.to(device)
+            recon_batch = model(data)
+            test_loss += loss_function(recon_batch, data).item()
+            if i == 0:
+                n = min(data.size(0), 8)
+                comparison = torch.cat([data[:n],
+                                      recon_batch.view(batch_size, 1, 28, 28)[:n]])
+                #save_image(comparison.cpu(), 'results/reconstruction_' + str(epoch) + '.png', nrow=n)
+    test_loss /= len(test_loader.dataset)
+    print('====> Test set loss: {:.4f}'.format(test_loss))
+
+def runit(epochs):
+    for epoch in range(1, epochs + 1):
+        train(epoch)
+        test(epoch)
+        with torch.no_grad():
+            sample = torch.randn(64, 20).to(device)
+            sample = model.decode(sample).cpu()
+            #save_image(sample.view(64, 1, 28, 28), 'results/sample_' + str(epoch) + '.png')
+
+
+runit(epochs)
+
