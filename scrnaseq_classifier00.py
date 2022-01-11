61b1f8402fc4587dc8783562b6363d51912d2659
Yiftach Kolb <zelhar@gmail.com>
2022-01-11 18:44:36 +0100

    classifier

--- scrnaseq_classifier00.py
+++ scrnaseq_classifier00.py
@@ -6,6 +6,7 @@ import numpy as np
 import torch
 import torch.utils.data
 import torchvision.utils as vutils
+import pandas as pd
 from math import pi, sin, cos, sqrt, log
 from toolz import partial, curry
 from torch import Tensor
@@ -22,6 +23,8 @@ from my_torch_utils import fclayer, init_weights
 from my_torch_utils import plot_images, save_reconstructs, save_random_reconstructs
 from my_torch_utils import fnorm, replicate, logNorm
 from my_torch_utils import scsimDataset
+
+import scsim.scsim as scsim
 
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -104,3 +107,80 @@ predicts = probs.argmax(axis=1)
 predicts
 ls
 
+### generate new simulated dataset
+simulator = scsim.scsim(ngenes=5000, nproggenes=300, ncells=10000,
+        ngroups=10, seed=42,libloc=7.64, libscale=0.78,
+        mean_rate=7.68, mean_shape=0.34, expoutprob=0.00286, expoutloc=6.15,
+        expoutscale=0.49, 
+        )
+
+ngenes = 10**4
+ncells = 10**4
+K=10
+deprob = .025
+progdeloc = deloc = deval = 1
+descale = 1.0
+progcellfrac = .35
+deprob = .025
+doubletfrac = .0
+ndoublets=int(doubletfrac*ncells)
+nproggenes = 400
+nproggroups = int(K/3)
+proggroups = list(range(1, nproggroups+1))
+randseed = 42
+
+simulator = scsim.scsim(
+    ngenes=ngenes,
+    ncells=ncells,
+    ngroups=K,
+    libloc=7.64,
+    libscale=0.78,
+    mean_rate=7.68,
+    mean_shape=0.34,
+    expoutprob=0.00286,
+    expoutloc=6.15,
+    expoutscale=0.49,
+    diffexpprob=deprob,
+    diffexpdownprob=0.0,
+    diffexploc=deloc,
+    diffexpscale=descale,
+    bcv_dispersion=0.448,
+    bcv_dof=22.087,
+    ndoublets=ndoublets,
+    nproggenes=nproggenes,
+    progdownprob=0.0,
+    progdeloc=progdeloc,
+    progdescale=descale,
+    progcellfrac=progcellfrac,
+    proggoups=proggroups,
+    minprogusage=0.1,
+    maxprogusage=0.7,
+    seed=randseed,
+    
+)
+
+simulator.simulate()
+
+simulator.cellparams.to_csv(path_or_buf="data/scrnasim/cellparams2.tsv.gz",
+        sep='\t',
+        compression="gzip",
+        )
+
+
+df = pd.read_csv("./data/scrnasim/cellparams2.tsv.gz",
+        sep='\t', index_col=0, compression='gzip', 
+        )
+
+df
+
+f = np.load("data/scrnasim/cellparams2.tsv", allow_pickle=True)
+
+df = scsim.load_df("data/scrnasim/cellparams.npz")
+
+df
+
+scsim.save_df(df, "data/scrnasim/blabla.gz")
+
+df = scsim.load_df("data/scrnasim/blabla.gz.npz")
+
+df
