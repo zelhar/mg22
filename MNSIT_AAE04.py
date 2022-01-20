190c3ac3a70fb98f908f0a59ffaf7c804cf88590
Yiftach Kolb <zelhar@gmail.com>
2022-01-20 16:54:01 +0100

    S

--- MNSIT_AAE04.py
+++ MNSIT_AAE04.py
@@ -16,6 +16,7 @@ from typing import Callable, Iterator, Union, Optional, TypeVar
 from typing import List, Set, Dict, Tuple
 from typing import Mapping, MutableMapping, Sequence, Iterable
 from typing import Union, Any, cast
+from torch.nn.functional import one_hot
 
 # my own sauce
 from my_torch_utils import denorm, normalize, mixedGaussianCircular
@@ -25,96 +26,10 @@ from my_torch_utils import fnorm, replicate, logNorm
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-class Encoder(nn.Module):
-    """
-    a gauusian encoder. using reparametrization trick.
-    """
-    def __init__(self, imgsize : int = 28, nz : int =2, nh : int = 1024) -> None:
-        super(Encoder, self).__init__()
-        self.nz = nz
-        self.imgsize = imgsize
-        self.nin = imgsize**2
-        nin = imgsize**2
-
-        self.encoder = nn.Sequential(
-                nn.Flatten(),
-                fclayer(nin, nh, False, 0, nn.LeakyReLU()),
-                fclayer(nin=nh, nout=nh, batchnorm=True, dropout=0.2,
-                    activation=nn.LeakyReLU()),
-        )
-
-        self.mu = nn.Linear(nh, nz)
-        self.logvar = nn.Linear(nh, nz)
-        return
-
-    def encode(self, x):
-        h = self.encoder(x)
-        mu = self.mu(h)
-        logvar = self.logvar(h)
-        return mu, logvar
-
-    def reparameterize(self, mu, logvar):
-        std = torch.exp(0.5 * logvar)
-        eps = torch.randn_like(std)
-        return mu + eps * std
-
-    def forward(self, x):
-        mu, logvar = self.encode(x)
-        z = self.reparameterize(mu, logvar)
-        return z
-
-class Decoder(nn.Module):
-    """
-    a deterministic decoder.
-    """
-    def __init__(self, imgsize : int = 28, nz : int =2, nh : int = 1024) -> None:
-        super(Decoder, self).__init__()
-        self.nz = nz
-        self.imgsize = imgsize
-        self.nin = imgsize**2
-        nin = imgsize**2
-
-        self.decoder = nn.Sequential(
-                fclayer(nin=nz, nout=nh, batchnorm=False, dropout=0,
-                    activation=nn.LeakyReLU()),
-                fclayer(nin=nh, nout=nh, batchnorm=True, dropout=0.2,
-                    activation=nn.LeakyReLU()),
-                fclayer(nin=nh, nout=nin, batchnorm=False, dropout=0,
-                    activation=nn.Tanh()),
-                nn.Unflatten(1, (1, imgsize, imgsize)),
-        )
-        return
-
-    def forward(self, z):
-        imgs = self.decoder(z)
-        return imgs
-
-
-class Discriminator(nn.Module):
-    def __init__(self, nin : int = 2, nh : int = 1024) -> None:
-        super(Discriminator, self).__init__()
-
-        self.main = nn.Sequential(
-                fclayer(nin=nin, nout=nh, batchnorm=False, dropout=0,
-                    activation=nn.LeakyReLU()),
-                fclayer(nin=nh, nout=nh, batchnorm=True, dropout=0.2,
-                    activation=nn.LeakyReLU()),
-                fclayer(nin=nh, nout=1, batchnorm=False, dropout=0,
-                    activation=nn.Sigmoid()),
-                )
-        return
-
-    def forward(self, z):
-        p = self.main(z)
-        return p
-
-
-
-
 
 transform = transforms.Compose([
     transforms.ToTensor(),
-    normalize,
+    #normalize,
     ])
 test_loader = torch.utils.data.DataLoader(
    dataset=datasets.MNIST(
@@ -137,35 +52,132 @@ train_loader = torch.utils.data.DataLoader(
    shuffle=True,
 )
 
-imgs, labels = test_loader.__iter__().next()
-imgs
-encoder = Encoder(28, 2, 1024)
-encoder(imgs).shape
-encoder.encode(imgs)
-decoder = Decoder(28, 2, 1024)
-decoder(encoder(imgs)).shape
-discriminator = Discriminator(2, 1024)
+class Encoder(nn.Module):
+    def __init__(self, imgsize=28, nh=1024, nz=10):
+        super(Encoder, self).__init__()
+        self.nin = nin = imgsize**2
+        self.nz = nz
+        self.imgsize = imgsize
 
-discriminator(encoder(imgs)).shape
+        self.encoder = nn.Sequential(
+                nn.Flatten(),
+                fclayer(nin, nh, False, 0, nn.LeakyReLU()),
+                fclayer(nin=nh, nout=nh, batchnorm=True, dropout=0.2,
+                    activation=nn.LeakyReLU()),
+                fclayer(nh, nh, True, 0.2, nn.LeakyReLU()),
+                fclayer(nh, nh, True, 0.2, nn.LeakyReLU()),
+        )
+
+        self.mu = nn.Linear(nh, nz)
+        self.logvar = nn.Linear(nh, nz)
+
+        return
+
+    def encode(self, x):
+        h = self.encoder(x)
+        mu = self.mu(h)
+        logvar = self.logvar(h)
+        return mu, logvar
+
+    def reparameterize(self, mu, logvar):
+        std = torch.exp(0.5 * logvar)
+        eps = torch.randn_like(std)
+        return mu + eps * std
+
+    def forward(self, x):
+        mu, logvar = self.encode(x)
+        z = self.reparameterize(mu, logvar)
+        return z
 
 
-encoder = Encoder(28, 2, 1024).to(device)
-encoder.apply(init_weights)
-optimEnc = optim.Adam(encoder.parameters(), lr=5e-4)
-decoder = Decoder(28, 2, 1024).to(device)
-decoder.apply(init_weights)
-optimDec = optim.Adam(decoder.parameters(), lr=5e-4)
-discriminator = Discriminator(2, 1024).to(device)
-discriminator.apply(init_weights)
-optimDis = optim.Adam(discriminator.parameters(), lr=5e-4)
-optimGen = optim.Adam(encoder.parameters(), lr=5e-4)
+class Decoder(nn.Module):
+    """
+    a deterministic decoder.
+    """
+    def __init__(self, imgsize : int = 28, nz : int = 10, nh : int = 1024) -> None:
+        super(Decoder, self).__init__()
+        self.nz = nz
+        self.imgsize = imgsize
+        self.nin = nin = imgsize**2
+
+        self.decoder = nn.Sequential(
+                fclayer(nin=nz, nout=nh, batchnorm=False, dropout=0,
+                    activation=nn.LeakyReLU()),
+                fclayer(nin=nh, nout=nh, batchnorm=True, dropout=0.2,
+                    activation=nn.LeakyReLU()),
+                fclayer(nh, nh, True, 0.2, nn.LeakyReLU()),
+                fclayer(nin=nh, nout=nin, batchnorm=False, dropout=0,
+                    activation=nn.Sigmoid()),
+                nn.Unflatten(1, (1, imgsize, imgsize)),
+        )
+        return
+
+    def forward(self, z):
+        imgs = self.decoder(z)
+        return imgs
+
+class Discriminator(nn.Module):
+    def __init__(self, nin : int = 10, nh : int = 1024) -> None:
+        super(Discriminator, self).__init__()
+
+        self.main = nn.Sequential(
+                fclayer(nin=nin, nout=nh, batchnorm=False, dropout=0,
+                    activation=nn.LeakyReLU()),
+                fclayer(nin=nh, nout=nh, batchnorm=True, dropout=0.2,
+                    activation=nn.LeakyReLU()),
+                fclayer(nh, nh, True, 0.2, nn.LeakyReLU()),
+                fclayer(nin=nh, nout=1, batchnorm=False, dropout=0,
+                    activation=nn.Sigmoid()),
+                )
+        return
+
+    def forward(self, z):
+        p = self.main(z)
+        return p
+
+class Critique(nn.Module):
+    def __init__(self, nin : int = 10, nh : int = 1024) -> None:
+        super(Critique, self).__init__()
+
+        self.main = nn.Sequential(
+                fclayer(nin=nin, nout=nh, batchnorm=False, dropout=0,
+                    activation=nn.LeakyReLU()),
+                fclayer(nin=nh, nout=nh, batchnorm=True, dropout=0.2,
+                    activation=nn.LeakyReLU()),
+                fclayer(nh, nh, True, 0.2, nn.LeakyReLU()),
+                nn.Linear(nh, 1),
+                )
+        return
+
+    def forward(self, z):
+        p = self.main(z)
+        return p
+
+encoder = Encoder(28, 1024, 10)
+decoder = Decoder(28, 10, 1024)
+discriminator = Discriminator(10, 1024)
 
 bce = nn.BCELoss(reduction="mean")
 mse = nn.MSELoss(reduction="mean")
 kld = lambda mu, logvar: -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
 
+encoder.to(device)
+encoder.apply(init_weights)
+optimEnc = optim.Adam(encoder.parameters(), lr=5e-4)
+
+decoder.to(device)
+decoder.apply(init_weights)
+optimDec = optim.Adam(decoder.parameters(), lr=5e-4)
+
+discriminator.to(device)
+discriminator.apply(init_weights)
+optimDis = optim.Adam(discriminator.parameters(), lr=5e-4)
+
+optimGen = optim.Adam(encoder.parameters(), lr=5e-4)
+
+
 # let the training begin
-for epoch in range(15):
+for epoch in range(45):
     for idx, (data, labels) in enumerate(train_loader):
         x = data.to(device)
         batch_size = x.shape[0]
@@ -213,101 +225,93 @@ for epoch in range(15):
                 loss_gen.item(),
             )
 
-
-# compare orignal/recons from test set
-imgs, labels = test_loader.__iter__().next()
-z = encoder(imgs.cuda())
-ximgs = decoder(z).cpu()
-
-def plot_images(imgs, nrow=16, transform=nn.Identity(), out=plt):
-    imgs = transform(imgs)
-    grid_imgs = make_grid(imgs, nrow=nrow).permute(1, 2, 0)
-    out.imshow(grid_imgs)
-
-fig, ax = plt.subplots(2,1)
-
-grid_imgs = make_grid(imgs, nrow=16).permute(1, 2, 0)
-grid_ximgs = make_grid(ximgs, nrow=16).permute(1, 2, 0)
-
-ax[0].imshow(denorm(grid_imgs))
-ax[1].imshow(denorm(grid_ximgs))
-
-# clustering
-fig, ax = plt.subplots()
 encoder.cpu()
 decoder.cpu()
 discriminator.cpu()
 
-xs , labels = test_loader.__iter__().next()
-zs = encoder(xs)
-z = zs.detach().numpy()
-x = z[:,0]
-y = z[:,1]
-for i in range(10):
-    ax.scatter(x[labels == i], y[labels == i], label=str(i))
-#ax.scatter(x,y, c=labels, label=labels)
+imgs, labels = test_loader.__iter__().next()
 
-ax.legend("0123456789")
+fig, ax = plt.subplots(2,2)
+
+grid_imgs = make_grid(imgs, nrow=16).permute(1,2,0)
+ax[0,0].imshow(grid_imgs)
+
+z = encoder(imgs)
+y = decoder(z)
+
+grid_y = make_grid(y, nrow=16).permute(1,2,0)
+ax[0,1].imshow(grid_y)
+
+tiny = 5e-1
+w = z + tiny * torch.rand_like(z) - tiny/2
+w = decoder(w)
+grid_w = make_grid(w, nrow=16).permute(1,2,0)
+ax[1,1].imshow(grid_w)
+
+
+plt.close()
+
+import torch.linalg as lalg
+
+from scipy import linalg as LA
+
+from torch.linalg import eigh
+
+cov = z @ z.t()
+
+eigvals, eigvects = LA.eigh(cov.detach().numpy())
+
+
+idx = np.argsort(eigvals)[::-1]
+evecs = eigvects[:,idx]
+evals = eigvals[idx]
+
+evecs.shape
+
+x = z.detach().numpy()
+
+a = evecs @ x
+
+xs = a[:,0]
+ys = a[:,1]
+
+plt.scatter(xs, ys)
+
+for i in range(10):
+    plt.scatter(xs[labels == i], ys[labels == i], label=str(i))
+
 
 plt.cla()
 
-class Encoder(nn.Module):
-    """
-    a deterministic encoder.
-    """
-    def __init__(self, imgsize : int = 28, nz : int =2, nh : int = 1024) -> None:
-        super(Encoder, self).__init__()
-        self.nz = nz
-        self.imgsize = imgsize
-        self.nin = imgsize**2
-        nin = imgsize**2
+m = distributions.OneHotCategorical(torch.ones(10))
+m.sample((3,))
 
-        self.encoder = nn.Sequential(
-                nn.Flatten(),
-                fclayer(nin, nh, False, 0, nn.LeakyReLU()),
-                fclayer(nin=nh, nout=nh, batchnorm=True, dropout=0.2,
-                    activation=nn.LeakyReLU()),
-                fclayer(nin=nh, nout=nz, batchnorm=False, dropout=0,
-                    activation=nn.Tanh()),
-        )
+one_hot(torch.LongTensor([1,1,0,2]))
+x=torch.LongTensor([1,1,0,2])
 
-        return
+import umap
 
-    def forward(self, x):
-        z = self.encoder(x)
-        return z
-
-gauss = mixedGaussianCircular(rho=0.01, sigma=0.6, k=10, j=0)
-mix = distributions.Categorical(torch.ones(10,))
-comp = distributions.Independent(gauss, 0)
-gmm = distributions.MixtureSameFamily(mix, comp)
-samples = gmm.sample((10000,))
-samples = gmm.sample((10000,)) * 0.2
-samples.shape
-samples = samples.cpu().numpy()
-x = samples[:,0]
-y = samples[:,1]
-plt.scatter(x,y)
-plt.legend(['star'])
+fit = umap.UMAP(n_components=2)
 
 
-encoder = Encoder(28, 2, 1024).to(device)
+encoder = Encoder(28, 1024, 2)
+decoder = Decoder(28, 2, 1024)
+discriminator = Critique(2, 1024)
+
+
+encoder.to(device)
 encoder.apply(init_weights)
 optimEnc = optim.Adam(encoder.parameters(), lr=5e-4)
-decoder = Decoder(28, 2, 1024).to(device)
+decoder.to(device)
 decoder.apply(init_weights)
 optimDec = optim.Adam(decoder.parameters(), lr=5e-4)
-discriminator = Discriminator(2, 1024).to(device)
+optimGen = optim.Adam(encoder.parameters(), lr=5e-4)
+discriminator.to(device)
 discriminator.apply(init_weights)
 optimDis = optim.Adam(discriminator.parameters(), lr=5e-4)
-optimGen = optim.Adam(encoder.parameters(), lr=5e-4)
-
-z_gauss = torch.randn((128,2))
-z_star = gmm.sample((128,))
-
 
 # let the training begin
-for epoch in range(15):
+for epoch in range(10):
     for idx, (data, labels) in enumerate(train_loader):
         x = data.to(device)
         batch_size = x.shape[0]
@@ -326,27 +330,24 @@ for epoch in range(15):
         encoder.eval()
         discriminator.zero_grad()
         z = encoder(x)
-        #z_gauss = torch.randn_like(z).to(device)
-        #z_star = gmm.sample((batch_size,)).to(device)
-        z_star = 0.2 * gmm.sample((batch_size,)).to(device)
-        real_labels = torch.ones(batch_size, 1).to(device)
-        fake_labels = torch.zeros_like(real_labels).to(device)
-        #pred_real = discriminator(z_gauss)
-        pred_real = discriminator(z_star)
-        pred_fake = discriminator(z)
-        loss_dis = bce(pred_real, real_labels) + bce(pred_fake, fake_labels)
+        z_gauss = torch.randn_like(z).to(device)
+        lossD_real = -discriminator(z_gauss).mean()
+        lossD_fake = discriminator(z).mean()
+        loss_dis = (lossD_fake + lossD_real)/2
         loss_dis.backward()
         optimDis.step()
+        # clip discriminator's weights
+        for p in discriminator.parameters():
+            p.data.clamp_(-0.01, 0.01)
         # train encoder as generator
-        encoder.train()
-        encoder.zero_grad()
-        discriminator.eval()
-        z = encoder(x)
-        real_labels = torch.ones(batch_size, 1).to(device)
-        pred_real = discriminator(z)
-        loss_gen = bce(pred_real, real_labels)
-        loss_gen.backward()
-        optimGen.step()
+        if idx % 5 == 0:
+            encoder.train()
+            encoder.zero_grad()
+            discriminator.eval()
+            z = encoder(x)
+            loss_gen = -discriminator(z).mean()
+            loss_gen.backward()
+            optimGen.step()
         if idx % 100 == 0:
             print(
                 "losses:\n",
@@ -358,22 +359,18 @@ for epoch in range(15):
                 loss_gen.item(),
             )
 
-# clustering
-fig, ax = plt.subplots()
 
-encoder.cpu()
-decoder.cpu()
-discriminator.cpu()
-
-xs , labels = test_loader.__iter__().next()
-zs = encoder(xs)
-z = zs.detach().numpy()
-x = z[:,0]
-y = z[:,1]
+imgs, labels = test_loader.__iter__().next()
+z = encoder(imgs)
+x = z.detach().numpy()
+xs = x[:,0]
+ys = x[:,1]
 for i in range(10):
-    ax.scatter(x[labels == i], y[labels == i], label=str(i))
-#ax.scatter(x,y, c=labels, label=labels)
+    plt.scatter(xs[labels == i], ys[labels == i], label=str(i))
 
-ax.legend("0123456789")
+plt.close()
 
-plt.cla()
+plt.scatter(xs, ys, c=labels)
+
+plt.legend("0123456789")
+
