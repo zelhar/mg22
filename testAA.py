6e4f1ea3ae3fe7192bf4402e46d4d58d1cfcc570
Yiftach Kolb <zelhar@gmail.com>
2022-01-24 23:33:08 +0100

    testAA

--- testAA.py
+++ testAA.py
@@ -42,6 +42,7 @@ class AE(nn.Module):
             nn.Linear(nh, nh),
             nn.ReLU(),
             nn.Linear(nh, nz),
+            #nn.Tanh(),
         )
 
         self.decoder = nn.Sequential(
@@ -142,3 +143,90 @@ ax.legend("0123456789")
 plt.cla()
 
 plt.close()
+
+
+l1norm = nn.L1Loss()
+
+xold, _ = train_loader.__iter__().next()
+xold = xold.to(device)
+
+#### next try
+model.to(device)
+for epoch in range(10):
+    for idx, (data, _) in enumerate(train_loader):
+        model.train()
+        model.requires_grad_(True)
+        optimizer.zero_grad()
+        batch_size = data.shape[0]
+        x = data.to(device)
+        xnoisy = x + torch.rand_like(x) * 5e-2
+        y, z = model(x)
+        ynoisy, znoisy = model(xnoisy)
+        loss = mse(y,x) + mse(ynoisy, xnoisy) + mse(z, znoisy)
+        loss.backward()
+        optimizer.step()
+        #xold = xold[:batch_size]
+        if xold.shape == x.shape and l1norm(x, xold) >= 1e-1:
+            optimizer.zero_grad()
+            x = data.to(device)
+            y, z = model(x)
+            w, v = model(xold.detach())
+            # distance 0.2 to 0.5 if they are disimilar
+            loss2 = nn.ReLU()(
+                    0.5 - l1norm(z,v)
+                    ) + mse(y, x) + mse(w, xold)
+            #loss2 += mse(y,x) + mse(w, xold)
+            loss2.backward()
+            optimizer.step()
+        xold = data.to(device)
+        if idx % 300 == 0:
+            print(
+                "losses:\n",
+                loss.item(),
+                loss2.item(),
+            )
+
+# clustering
+model.cpu()
+
+fig, ax = plt.subplots()
+
+for idx, (imgs, labels) in enumerate(test_loader):
+    y, z = model(imgs)
+    x = z.detach().numpy()
+    xs = x[:,0]
+    ys = x[:,1]
+    for i in range(10):
+        ax.scatter(xs[labels == i], ys[labels == i], label=str(i))
+
+ax.legend("0123456789")
+
+plt.cla()
+
+plt.close()
+
+plt.savefig('./results/clustering_aae_tanh_close_and_far_training(testAA).png')
+
+plt.savefig('./results/clustering_aae_linear_(testAA).png')
+
+fig, ax = plt.subplots(2,1)
+
+imgs, labels = test_loader.__iter__().next()
+x, z = model(imgs)
+grid_imgs = make_grid(imgs, nrow=16).permute(1, 2, 0)
+grid_x = make_grid(x, nrow=16).permute(1, 2, 0)
+ax[0].imshow(grid_imgs)
+ax[1].imshow(grid_x)
+
+plt.savefig('./results/reconstruction_aae_linear_(testAA).png')
+
+zz = torch.randn_like(z)
+xx = model.decode(zz)
+grid_xx = make_grid(xx, nrow=16).permute(1, 2, 0)
+
+ax[0].imshow(grid_xx)
+
+plt.savefig('./results/random_normal_reconstruction(top)_aae_linear_(testAA).png')
+
+plt.close()
+
