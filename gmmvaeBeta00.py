0c8fcd1c2daf986059771be273bbf04d4addcebe
Yiftach Kolb <zelhar@gmail.com>
2022-11-07 17:22:25 +0100

    S

--- gmmvaeBeta00.py
+++ gmmvaeBeta00.py
@@ -628,6 +628,197 @@ class VAE_AE_TypeB1500vC(nn.Module):
         output["losses"] = losses
         return output
 
+class VAE_AE_TypeB1500vC2(nn.Module):
+    """
+    conditional version of vanilla VAE
+    """
+
+    def __init__(
+        self,
+        nx: int = 28 ** 2,
+        nh: int = 1024,
+        nhq: int = 1024,
+        nhp: int = 1024,
+        nz: int = 64,
+        zscale: float = 1e0,
+        numhidden: int = 2,
+        numhiddenq: int = 2,
+        numhiddenp: int = 2,
+        dropout: float = 0.3,
+        bn: bool = True,
+        reclosstype: str = "Gauss",
+        use_resnet: bool = False,
+        softargmax: bool = False,
+        eps: float = 1e-9,
+        restrict_z: Union[bool, str] = False,
+        activation=nn.LeakyReLU(),
+        nc1: int = 5,
+        learned_prior: bool = False,
+    ) -> None:
+        super().__init__()
+        self.nx = nx
+        self.nh = nh
+        self.nhq = nhq
+        self.nhp = nhp
+        self.nz = nz
+        self.eps = eps
+        self.numhidden = numhidden
+        self.numhiddenq = numhiddenq
+        self.numhiddenp = numhiddenp
+        self.zscale = zscale
+        self.restrict_z = restrict_z
+        self.softargmax = softargmax
+        self.use_resnet = use_resnet
+        self.logsigma_x = torch.nn.Parameter(torch.zeros(nx), requires_grad=True)
+        self.reclosstype = reclosstype
+        self.nc1 = nc1
+        self.learned_prior = learned_prior
+        self.kld_unreduced = lambda mu, logvar: -0.5 * (
+            1 + logvar - mu.pow(2) - logvar.exp()
+        )
+        self.z_prior = distributions.Normal(
+            loc=torch.zeros(nz),
+            scale=torch.ones(nz),
+        )
+        ## P network
+        self.Px = ut.buildNetworkv5(
+            [nz] + numhiddenp * [nhp] + [nx],
+            #[nz + nc1] + numhiddenp * [nhp] + [nx],
+            dropout=dropout,
+            activation=activation,
+            batchnorm=bn,
+        )
+        # z prior
+        self.Pz = ut.buildNetworkv5(
+            [nc1] + numhiddenq * [nhq] + [2 * nz],
+            activation=activation,
+            batchnorm=bn,
+        )
+        ## Q network
+        self.Qz = ut.buildNetworkv5(
+            [nx + nc1] + numhiddenq * [nhq] + [2 * nz],
+            dropout=dropout,
+            activation=activation,
+            batchnorm=bn,
+        )
+        return
+
+    def printDict(self, d: dict):
+        for k, v in d.items():
+            print(k + ":", v.item())
+        return
+
+    def forward(
+        self,
+        input,
+        cond1=None,
+        y=None,
+    ):
+        x = nn.Flatten()(input)
+        batch_size = x.shape[0]
+        mse = nn.MSELoss(reduction="none")
+        if cond1 == None:
+            cond1 = torch.zeros((batch_size, self.nc1), device=x.device)
+        losses = {}
+        output = {}
+        eps = self.eps
+        wz = self.Qz(
+            torch.cat(
+                [
+                    x,
+                    cond1,
+                ],
+                dim=-1,
+            )
+        )
+        mu_z = wz[:, : self.nz]
+        logvar_z = wz[:, self.nz :]
+        if self.restrict_z == "lv":
+            logvar_z = logvar_z.tanh() * 5
+        elif self.restrict_z == "mlv":
+            mu_z = mu_z.tanh()
+            logvar_z = logvar_z.tanh() * 5
+        elif self.restrict_z:
+            mu_z = ut.softclip(mu_z, -5, 5)
+            logvar_z = ut.softclip(logvar_z, -9, 1)
+        else:
+            pass
+        std_z = (0.5 * logvar_z).exp()
+        noise = torch.randn_like(mu_z).to(x.device)
+        z = mu_z + noise * std_z
+        Qz = distributions.Normal(loc=mu_z, scale=std_z)
+        output["Qz"] = Qz
+        output["mu_z"] = mu_z
+        output["logvar_z"] = logvar_z
+        output["z"] = z
+        rec = self.Px(
+            torch.cat(
+                [
+                    z,
+                    #cond1,
+                ],
+                dim=1,
+            )
+        )
+        if self.reclosstype == "Bernoulli":
+            logits = rec
+            rec = logits.sigmoid()
+            bce = nn.BCEWithLogitsLoss(reduction="none")
+            loss_rec = bce(logits, x).sum(-1).mean()
+        elif self.reclosstype == "mse":
+            mse = nn.MSELoss(reduction="none")
+            loss_rec = mse(rec, x).sum(-1).mean()
+        else:
+            logsigma_x = ut.softclip(self.logsigma_x, -8, 8)
+            sigma_x = logsigma_x.exp()
+            Qx = distributions.Normal(loc=rec, scale=sigma_x)
+            loss_rec = -Qx.log_prob(x).sum(-1).mean()
+        output["rec"] = rec
+        losses["rec"] = loss_rec
+
+        z_mu_logvar_prior = self.Pz(
+            torch.cat(
+                [
+                    cond1,
+                ],
+                dim=-1,
+            )
+        )
+        mu_z_prior = z_mu_logvar_prior[:, : self.nz]
+        logvar_z_prior = z_mu_logvar_prior[:, self.nz :]
+        if self.restrict_z == "lv":
+            logvar_z_prior = logvar_z_prior.tanh() * 5
+        elif self.restrict_z == "mlv":
+            mu_z_prior = mu_z_prior.tanh()
+            logvar_z_prior = logvar_z_prior.tanh() * 5
+        elif self.restrict_z:
+            mu_z_prior = ut.softclip(mu_z_prior, -5, 5)
+            logvar_z_prior = ut.softclip(logvar_z_prior, -9, 1)
+        else:
+            pass
+        if not self.learned_prior:
+            loss_z = (
+                self.zscale
+                * self.kld_unreduced(mu=mu_z, logvar=logvar_z).sum(-1).mean()
+            )
+        else:
+            loss_z = (
+                self.zscale
+                * ut.kld2normal(
+                    mu=mu_z,
+                    logvar=logvar_z,
+                    mu2=mu_z_prior,
+                    logvar2=logvar_z_prior,
+                )
+                .sum(-1)
+                .mean()
+            )
+        losses["loss_z"] = loss_z
+        total_loss = loss_rec + loss_z
+        losses["total_loss"] = total_loss
+        output["losses"] = losses
+        return output
+
 class VAE_TypeB1601(nn.Module):
     """
     VAE with 'generating' variable w and latent/reduced variable z.
