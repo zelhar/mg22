3cbae76ec856a114bda3258da11787d560fc8973
Yiftach Kolb <zelhar@gmail.com>
2023-01-12 17:29:15 +0100

    S

--- unrelated/laws.hs
+++ unrelated/laws.hs
@@ -1,7 +1,7 @@
 --import Data.Functor
-
 import Control.Monad
 import Control.Applicative
+import Language.Haskell.TH (thisModule)
 
 
 
@@ -52,6 +52,19 @@ If f is also a Monad then it must hold
 pure = return
 (*>) = (>>)
 m1 <*> m2 = m1 >>= (\x1 -> m2 >>= (\x2 -> return (x1 x2)))
+
+
+## Alternative
+class Applicative f => Alternative f where
+a monoid on applicative functorsv
+empty :: f a
+(<|>) :: f a -> f a -> f a
+some :: f a -> f [a] --one or more
+many :: f a -> f [a] --zero or more
+
+if defines, some and many should be the least soloutions for
+some v = (:) <$> v <*> many v
+many v = some v <|> pure []
 -}
 
 x :: Maybe Int
@@ -63,8 +76,43 @@ res = bar <*> [1]
 
 {-
 ## Monad
-}-
 
+-}
+
+divisors :: Integer -> [Integer]
+divisors n = [k | k<-[1..l], n `mod` k == 0] 
+  where
+     l = round $ sqrt $ fromInteger n
+
+
+alter = (=<<) divisors
+
+alter' :: [Integer] -> [Integer]
+alter' ls = do {
+               x <- ls;
+               divisors x;
+               }
+
+fooo :: [Integer] -> Integer -> [Integer] 
+fooo xs n = do 
+  { x<-xs
+  ; if mod n x == 0 then 
+                    return x;
+                    else do [];
+  }
+
+
+
+divisors' :: [Integer] -> [[Integer]]
+divisors' ns = do {
+                  n <- ns;
+                  [divisors n];
+                  }
+
+isit x =
+  if x == 0
+    then 0
+    else 2
 
 
 
