8f97a1a12975b07028aeefdbabaefd9a721a2400
Yiftach Kolb <zelhar@gmail.com>
2023-01-19 18:19:06 +0100

    S

--- unrelated/fib.hs
+++ unrelated/fib.hs
@@ -12,6 +12,32 @@ fibAkk 0 a b = (a, b, 0)
 fibAkk 1 a b = (a, b, 0)
 fibAkk n a b = fibAkk (n-1) (a+b) a
 
+isPrime :: Integer -> Bool
+isPrime 0 = False
+isPrime 1 = True
+isPrime 2 = True
+isPrime 3 = True
+isPrime 4 = False
+isPrime x = let { n = round $ sqrt $ fromIntegral x
+                ; l = [(mod x i) | i <- [2..n] ]
+                ; ll = dropWhile (\k -> k>0) l
+                }
+             in ll == []
+
+findPrime :: [Int] -> Int -> Bool
+findPrime [] _ = True
+findPrime l n = (mod n (head l) /= 0) && findPrime (tail l) n 
+
+isPrime' :: Int -> Bool
+isPrime' n = findPrime [2..x] n where
+  x = round $ sqrt $ fromIntegral n
+
+
+listPrimes :: Integer -> [Integer]
+listPrimes n = [x | x <- [1..n], isPrime(x)]
+
+listPrimes' :: Int -> [Int]
+listPrimes' n = [x | x <- [1..n], isPrime'(x)]
 
 main :: IO ()
 main = do
--- unrelated/justify.hs
+++ unrelated/justify.hs
@@ -1,7 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
 import Control.Monad
 import Control.Applicative
---import System.IO
+import System.IO
 import Data.Char
+import System.Environment
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import Data.Semigroup
 --import Data.String
 
 --main :: IO ()
@@ -15,14 +20,58 @@ import Data.Char
 --          putStrLn ("hello " ++ y);
 --          }
 --
-mains =
- do name <- getLine
-    let loudName = map toUpper name
-    putStrLn ("Hello " ++ loudName ++ "!")
-    putStrLn ("Oh boy! Am I excited to meet you, " ++ loudName)
+--
 
-mainss = do { name <- getLine
-            ; let loudName = map toUpper name
-            ; putStrLn ("Hello " ++ loudName ++ "!");
-            }
+-- The plan:
+-- strip white space
+-- split lines
+-- take paragraph, justify, repeat
+
+--justifyParagraph :: Int -> [T.Text] -> T.Text
+
+
+getOutputFile :: String -> IO Handle
+getOutputFile path = do { hand <- openFile path WriteMode
+                        ; isOK <- hIsWritable hand
+                        ; if and [isOK, path /= "-"]  then return hand else return stdout
+                        }
+
+main = 
+  do { args <- getArgs
+     ; text <- TIO.getContents
+     ; let minitext = T.takeWhile (/='ü') text
+     ; TIO.putStrLn $ T.strip $ T.toUpper minitext
+     ; let myLines = map T.strip (T.lines text)
+     ; TIO.putStrLn $ T.unlines myLines
+     }
+
+mainOld =
+ do { args <- getArgs
+    ; let path = if null args then "-" else head args
+    ; hand <- getOutputFile path
+    --; hand <- openFile (head args) AppendMode
+    --; hPutStrLn hand "fuck off!"
+    ; print hand
+    ; do { test <- hIsWritable hand
+         ; print "is writeable?"
+         ; print test
+         }
+    ; do { test <- hIsReadable hand
+         ; print "is readable?"
+         ; print test
+         }
+    ; print "is handle stdout?"
+    ; print (stdout == hand)
+    ; hPutStrLn stdout (show args)
+    ; name <- getLine
+    ; let loudName = map toUpper name
+    ; putStrLn ("Hello " ++ loudName ++ "!")
+    ; putStrLn ("Oh boy! Am I excited to meet you, " ++ loudName)
+    ;hClose hand
+    }
+
+--mainss = do { name <- getLine
+--            ; let loudName = map toUpper name
+--            ; putStrLn ("Hello " ++ loudName ++ "!");
+--            }
             
